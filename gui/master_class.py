f59fdac63977e7bdf3ee67985b54e0a77f20c665
Szymon Ciemała <szymciem8@gmail.com>
2020-10-16 13:08:41 +0200

    Update master_class.py
    update

--- gui/master_class.py
+++ gui/master_class.py
@@ -1,14 +1,25 @@
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
+import os
 
+program_name = "Stock Analizer"
+window_width = 500
+window_height = 500
+window_x = 500
+window_y = 100
+
+#Tworzymy klasę dziedziczącą od klasy QMainWindow
 class MyWindow(QMainWindow):
     def __init__(self):
-        super(MyWindow, self).__init__()
-        self.setGeometry(0, 0, 300, 300)
+        super(MyWindow, self).__init__() #Rozszerzamy funkcję inicjalizującą
+        self.setGeometry(window_x, window_y, window_width, window_height)
         self.setWindowTitle("My Window!")
         self.initUI()
 
+    #Incjalizujemy UI
     def initUI(self):
         self.label = QtWidgets.QLabel(self)
         self.label.setText("My first label!")
@@ -21,11 +32,22 @@ class MyWindow(QMainWindow):
     def clicked(self):
         self.label.setText("You pressed the button")
 
-def window():
+    def draw_diagram(self):
+        self.graphWidget = pg.PlotWidget()
+        self.setCentralWidget(self.graphWidget)
+
+        #Dane testowe
+        hour = [i for i in range(10)]
+        temp = [i**2 for i in range(10)]
+
+        self.graphWidget.plot(hour, temp)
+
+def main():
     app = QApplication(sys.argv)
     win = MyWindow()
-
+    win.draw_diagram()
     win.show()
     sys.exit(app.exec_())
 
-window()
+if __name__ == '__main__':
+    main()
